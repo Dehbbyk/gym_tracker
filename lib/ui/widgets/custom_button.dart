3777d3731b2f7dcf1858207ad67d0ca7d5141fc2
damilare-d <damilareakinwale0@gmail.com>
2024-07-30 04:36:45 +0100

    refactored widget

--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -1,37 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gym_tracker/ui/common/app_colors.dart';
 
 class CustomButton extends StatelessWidget {
   final String buttonText;
   final IconData? icon;
   final void Function()? onPressed;
+  final double? height;
 
   const CustomButton(
-      {super.key, required this.buttonText, this.icon, this.onPressed});
+      {super.key,
+      required this.buttonText,
+      this.icon,
+      this.onPressed,
+      this.height});
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       onPressed: onPressed,
-      color: const Color(0xffF97316),
+      color: kcPrimaryColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(6),
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 95.0, right: 100.0),
-        child: Row(
-          children: [
-            Icon(icon),
-            Text(
-              buttonText,
-              style: GoogleFonts.inter(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: const Color(0xffFFFFFF),
-              ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (icon != null) Icon(icon),
+          Text(
+            buttonText,
+            style: GoogleFonts.inter(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: const Color(0xffFFFFFF),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }

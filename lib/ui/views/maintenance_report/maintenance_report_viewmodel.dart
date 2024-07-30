ae796e9fbc72f1ae5a6ec146a28aafe605d5ac78
damilare-d <damilareakinwale0@gmail.com>
2024-07-30 15:49:17 +0100

    fixes : copied and pasted from another branch

--- lib/ui/views/maintenance_report/maintenance_report_viewmodel.dart
+++ lib/ui/views/maintenance_report/maintenance_report_viewmodel.dart
@@ -1,3 +1,31 @@
+import 'package:gym_tracker/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class MaintenanceReportViewModel extends BaseViewModel {}
+import '../../../app/app.locator.dart';
+
+class MaintenanceReportViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  int noteIndex = 0;
+
+  List<String> onboardingNotes = [
+    "assets/images/equip_detail.png",
+    "assets/images/equip_detail.png",
+    "assets/images/equip_detail.png",
+    "assets/images/equip_detail.png",
+    "assets/images/equip_detail.png",
+  ];
+
+  void goBack() => _navigationService.back();
+
+  void logWorkoutNavigation() => _navigationService.navigateToLogWorkoutView();
+  void reportIssuesNavigation() =>
+      _navigationService.navigateToReportIssuesView();
+  void maintenanceReportNavigation() =>
+      _navigationService.navigateToMaintenanceView();
+
+  void changeNote(int index) {
+    noteIndex = index;
+    notifyListeners();
+  }
+}
